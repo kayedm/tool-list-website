a2e4034af06014a2660e13ce744527c40a27a200
Kayed Mankal <mank0040@algonquinlive.com>
2025-03-26 16:40:11 -0400

    Added search bar, sign in, login validation

--- js/script.js
+++ js/script.js
@@ -1,94 +1,108 @@
-/*This function toggles between darkmode and light mode*/
+// TODO
+// 1. actively listen for user input and clear error messages as the user corrects their input.
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('DOM loaded');
+
+    const loginform = document.getElementById('loginform');
+    const signupform = document.getElementById('signupform');
+
+    /* login form get docs */
+    const loginEmail = loginform.querySelector('[name="email"]');
+    const loginPass = loginform.querySelector('[name="password"]');
+
+    /* signin form get docs */
+    const signupName = signupform.querySelector('[name="name"]');
+    const signupEmail = signupform.querySelector('[name="email"]');
+    const signupPass = signupform.querySelector('[name="password"]');
+    const signupPass2 = signupform.querySelector('[name="password2"]');
+
+    const showError = (element, message) => {
+        const field = element.parentElement;
+        const errorBox = field.querySelector('.error');
+        errorBox.innerText = message;
+    };
+
+    const showSuccess = (element) => {
+        const field = element.parentElement;
+        const errorBox = field.querySelector('.error');
+        errorBox.innerText = '';
+    };
+
+    const validateLogin = () => {
+        let success = true;
+
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(loginEmail.value)) {
+            showError(loginEmail, 'Email address should be non-empty with the format xyx@xyx.xyx.');
+            success = false;
+        }   else {
+            showSuccess(loginEmail);
+        }
+
+        if (loginPass.value.length <= 8) {
+            showError(loginPass, 'Password should be at least 8 charaters long.');
+            success = false;
+        }   else {
+            showSuccess(loginPass);
+        }
+
+        return success;
+    };
+
+    const validateSignup = () => {
+        let signupSuccess = true;
+
+        if (signupName.value.trim() === "") {
+            showError(signupName, 'Name should be non-empty.');
+            signupSuccess = false;
+        }   else {
+            showSuccess(signupName);
+        }
+
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(signupEmail.value)) {
+            showError(signupEmail, 'Email address should be non-empty with the format xyx@xyx.xyx.');
+            signupSuccess = false;
+        }   else {
+            showSuccess(signupEmail);
+        }
+
+        if (signupPass.value.length <= 8) {
+            showError(signupPass, 'Password should be at least 8 charaters long.');
+            signupSuccess = false;
+        }   else {
+            showSuccess(signupPass);
+        }
+
+        if (signupPass2.value !== signupPass.value) {
+            showError(signupPass2, 'Please retype password.');
+            signupSuccess = false;
+        }   else {
+            showSuccess(signupPass2);
+        } 
+
+        return signupSuccess;
+
+    };
+
+
+    loginform.addEventListener('submit', function (e) {
+            console.log('submitting login form...');
+            if (!validateLogin()) {
+                e.preventDefault();
+            }
+        });
+        
+    signupform.addEventListener('submit', function (e) {
+            console.log('submitting signup form...');
+            if (!validateSignup()) {
+                e.preventDefault();
+            }
+        });        
+
+});
+
 function toggleDarkMode() {
-    const body = document.body;
-    const header = document.querySelector('header');
-    
-    body.classList.toggle('dark-mode');
-    header.classList.toggle('dark-mode');
-};
-
-const popoutButton = document.getElementById("popout-button");
-const popoutForm = document.getElementById("popout-form");
-const closeButton = document.getElementById("close-popout");
-
-/*This function opens the opens the sign in popup form*/ 
-popoutButton.addEventListener("click", () => {
-  popoutForm.classList.add("active");
-});
-
-/*This function closes the sign in popup form*/ 
-closeButton.addEventListener("click", () => {
-  popoutForm.classList.remove("active");
-});
-
-/*This function will close the popup if mouse is clicked outside of the popup area*/
-document.addEventListener("click", (e) => {
-  if (!popoutForm.contains(e.target) && e.target !== popoutButton) {
-    popoutForm.classList.remove("active");
-  }
-});
-
-const email = document.getElementById('email');
-const password = document.getElementById('pass');
-const password2 = document.getElementById('pass2');
-const checkBox = document.getElementById('terms');
-const form = document.getElementById('form');
-
-
-/*This function prevents form submission in order to validate*/
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); 
-
-    validateInputs();
-});
-
-const setError = (element, message) => {
-    const formcontainer = element.parentElement;
-    const errorDisplay = formcontainer.querySelector('.error');
-
-    errorDisplay.innerText = message;
-    formcontainer.classList.add('error');
-    formcontainer.classList.remove('success');
-};
-
-const setSuccess = (element) => {
-    const formcontainer = element.parentElement;
-    const errorDisplay = formcontainer.querySelector('.error');
-
-    errorDisplay.innerText = ''; 
-    formcontainer.classList.add('success');
-    formcontainer.classList.remove('error');
-};
-
-/*This function validates the inputs*/
-function validate() {
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-    const checkBoxValue = checkBox.value.trim();
-
-    if (emailValue === '' || !/\S+@\S+\.\S+/.test(emailValue)) {
-        setError(email, 'X Enter a valid email (xxx@yyy.zzz)');
-    } else {
-        setSuccess(email);
-    }
-
-    if (passwordValue === '' || passwordValue.length < 6 || !/[A-Z]/.test(passwordValue) || !/[a-z]/.test(passwordValue)) {
-        setError(password, 'X Password must be at least 6 characters with 1 uppercase & 1 lowercase letter');
-    } else {
-        setSuccess(password);
-    }
-
-    if (password2Value !== passwordValue || password2Value === '') {
-        setError(password2, 'X Passwords do not match');
-    } else {
-        setSuccess(password2);
-    }
-
-    if (checkBox.checked == false) {
-        setError(checkBox, 'X Please agree to the terms')
-    }   else {
-        setSuccess(checkBox);
-    }
-};
- 
+    document.body.classList.toggle("dark-mode");
+}
